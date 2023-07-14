59af0fa157bf7673b5dcfb84b9244ca0add6b52a
Dillon Vu (dtv782) <vutuanduong2003@yahoo.com>
2023-07-14 00:00:51 -0600

    add mysql init

---
+++ mysql-init/products-init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS nomosnow;
+USE nomosnow;
+CREATE TABLE IF NOT EXISTS products (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    barcode VARCHAR(255),
+    description VARCHAR(MAX),
+    status VARCHAR(10) NOT NULL
+);
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
+FLUSH PRIVILEGES;
